8fa713a5dae05531feff50bb07ff78b3230c9b06
cah6 <christian.henry92@gmail.com>
2014-06-12 18:02:56 -0500

    made edit window, trying to add it to edit button

---
+++ src/view/PlateEditing.java
@@ -0,0 +1,22 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * Created by Christian on 6/12/2014.
+ */
+public class PlateEditing extends JPanel {
+    private JTextField wellPosXTxt;
+    private JTextField wellPosYTxt;
+    private JTextField wellToWellDiameterTxt;
+    private JTextField numRowsTxt;
+    private JTextField numColsTxt;
+    private JTextField widthTxt;
+    private JTextField lengthTxt;
+    private JTextField wellDiameterTxt;
+    private JTextField wellVolumeTxt;
+    private JTextField wellHeightTxt;
+    private JButton clearAllFieldsButton;
+    private JButton saveAsButton;
+    private JTextField textField1;
+}
